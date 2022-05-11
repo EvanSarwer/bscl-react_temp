b4a2b75b0a264c1d0e80660aa07466bfe59f157a
shasabbir <shasabbir234@gmail.com>
2022-05-11 16:19:13 +0600

    Tables added and graph repositioned

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import CurrentStatus from "../CurrentStatus/CurrentStatus";
 import Graph from "../Graph/Graph";
 import ReachGraph from "../Graph/ReachGraph";
+import ActiveChannelTable from "../Table/ActiveChannelTable";
+
 
 const Dashboard =()=>{
     return(
@@ -12,10 +14,36 @@ const Dashboard =()=>{
                     <div class="content-body">
 
                         <CurrentStatus/>
+                        <div class="row">
+                        <div class="col-md-8">
+                        <ActiveChannelTable/>
+                        </div>
                         
-                        <div class="row match-height"><ReachGraph/></div>
+                        <div class="col-md-4">
+                        <ActiveChannelTable/>
+                        </div>
+                        </div>
                         
-                        <Graph/>
+                        <div class="row">
+                        
+                        <div class="col-md-6">
+                        <ReachGraph/>
+                        </div>
+                        <div class="col-md-6">
+                        <ReachGraph/>
+                        </div>
+                        </div>
+                        <div class="row">
+                        
+                        <div class="col-md-6">
+                        <ReachGraph/>
+                        </div>
+                        <div class="col-md-6">
+                        <ReachGraph/>
+                        </div>
+                        </div>
+                        
+                        
 
 
 
--- src/Components/Graph/ReachGraph.js
+++ src/Components/Graph/ReachGraph.js
@@ -197,29 +197,19 @@ type:"bar"
     return (
         <div>
         
-    <div class="row">
-    <div class="col-12">
+    
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title">Column Chart</h4>
-                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-                <div class="heading-elements">
-                    <ul class="list-inline mb-0">
-                        <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-                        <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-                        <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-                        <li><a data-action="close"><i class="ft-x"></i></a></li>
-                    </ul>
-                </div>
+                
             </div>
             <div class="card-content collapse show">
                 <div class="card-body">
-                    <canvas id="column-chart" height="400" width="400"></canvas>
+                    <canvas id="column-chart" height="400"></canvas>
                 </div>
             </div>
         </div>
     </div>
-    </div></div>
     )
 } 
 export default Graph;
