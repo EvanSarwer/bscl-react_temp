f4837b3ba2e4e9e5921a66e9734d80d0343863f9
shasabbir <shasabbir234@gmail.com>
2022-06-16 11:33:27 +0600

    menu edited

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -4,37 +4,37 @@ import { useState, useEffect } from "react";
 
 const MainMenu = () => {
 
-    const open_channel_menu=()=>{
-        if(document.querySelector("#channelmenu").innerHTML==""){
+    // const open_channel_menu = () => {
+    //     if (document.querySelector("#channelmenu").innerHTML == "") {
 
-            document.querySelector("#channelmenu").innerHTML=`<ul class="menu-content">
-            <li><a class="menu-item" href="/channelstatus"><i></i><span data-i18n="Crypto">General</span></a>
-            </li>
-            <li><a class="menu-item" href="/definedchannelstatus"><i></i><span data-i18n="Crypto">User Defined</span></a>
-            </li>
-        </ul>`
-        }
-        else{
-            document.querySelector("#channelmenu").innerHTML="";
-        }
-        
-    }
+    //         document.querySelector("#channelmenu").innerHTML = `<ul class="menu-content">
+    //         <li><a class="menu-item" href="/channelstatus"><i></i><span data-i18n="Crypto">General</span></a>
+    //         </li>
+    //         <li><a class="menu-item" href="/definedchannelstatus"><i></i><span data-i18n="Crypto">User Defined</span></a>
+    //         </li>
+    //     </ul>`
+    //     }
+    //     else {
+    //         document.querySelector("#channelmenu").innerHTML = "";
+    //     }
 
-    const open_user_menu=()=>{
-        if(document.querySelector("#usermenu").innerHTML==""){
+    // }
 
-            document.querySelector("#usermenu").innerHTML=`<ul class="menu-content">
-            <li><a class="menu-item" href="/userstatus"><i></i><span data-i18n="Crypto">General</span></a>
-            </li>
-            <li><a class="menu-item" href="/userdefined"><i></i><span data-i18n="Crypto">User Defined</span></a>
-            </li>
-        </ul>`
-        }
-        else{
-            document.querySelector("#usermenu").innerHTML="";
-        }
-        
-    }
+    // const open_user_menu = () => {
+    //     if (document.querySelector("#usermenu").innerHTML == "") {
+
+    //         document.querySelector("#usermenu").innerHTML = `<ul class="menu-content">
+    //         <li><a class="menu-item" href="/userstatus"><i></i><span data-i18n="Crypto">General</span></a>
+    //         </li>
+    //         <li><a class="menu-item" href="/userdefined"><i></i><span data-i18n="Crypto">User Defined</span></a>
+    //         </li>
+    //     </ul>`
+    //     }
+    //     else {
+    //         document.querySelector("#usermenu").innerHTML = "";
+    //     }
+
+    // }
     return (
         // <!-- BEGIN: Main Menu-->
 
@@ -47,11 +47,24 @@ const MainMenu = () => {
                     </li>
                     <li class=" nav-item"><a href="/livechannels"><i class="la la-tencent-weibo"></i><span class="menu-title" data-i18n="Templates">Live Channels</span></a>
                     </li>
-                    <li class=" nav-item"><a onClick={open_user_menu} ><i class="la la-user"></i><span class="menu-title" data-i18n="Dashboard">Users</span><span class="badge badge badge-info badge-pill float-right mr-2"></span></a>
-                        <div id="usermenu"></div>
+                    <li class=" nav-item"><a  ><i class="la la-user"></i><span class="menu-title" data-i18n="Dashboard">Users</span><span class="badge badge badge-info badge-pill float-right mr-2"></span></a>
+                        {/* <div id="usermenu"></div> */}
+                        <ul class="menu-content">
+                            <li><a class="menu-item" href="/userstatus"><i></i><span data-i18n="Crypto">General</span></a>
+                            </li>
+                            <li><a class="menu-item" href="/userdefined"><i></i><span data-i18n="Crypto">User Defined</span></a>
+                            </li>
+                        </ul>
+
                     </li>
-                    <li class=" nav-item"><a onClick={open_channel_menu} ><i class="la la-television"></i><span class="menu-title" data-i18n="Templates">Trend Analysis</span></a>
-                        <div id="channelmenu"></div>
+                    <li class=" nav-item"><a  ><i class="la la-television"></i><span class="menu-title" data-i18n="Templates">Trend Analysis</span></a>
+                        
+                        <ul class="menu-content">
+                            <li><a class="menu-item" href="/channelstatus"><i></i><span data-i18n="Crypto">General</span></a>
+                            </li>
+                            <li><a class="menu-item" href="/definedchannelstatus"><i></i><span data-i18n="Crypto">User Defined</span></a>
+                            </li>
+                        </ul>
                     </li>
                     <li class=" nav-item"><a href="/downloadreport"><i class="la la-download"></i><span class="menu-title" data-i18n="Templates">Report Generation</span></a>
                     </li>
