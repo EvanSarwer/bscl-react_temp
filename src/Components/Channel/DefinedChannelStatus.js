8397251e0bd0d219ed2f1d793a2f0b5096c8890f
shasabbir <shasabbir234@gmail.com>
2022-06-19 13:54:38 +0600

    day part api updated for graphs

--- src/Components/Channel/DefinedChannelStatus.js
+++ src/Components/Channel/DefinedChannelStatus.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import axiosConfig from "../axiosConfig";
+
+import BarGraph from "../Graph/BarGraph";
 import Select from 'react-select';
 
 
@@ -16,69 +18,46 @@ const ChannelStatus = () => {
     const [start, setstart] = useState("");
     const [finish, setfinish] = useState("");
     const [channels, setchannels] = useState([]);
-    useEffect(() => {
 
-        axiosConfig.get("/api/trend/channels").then(rsp => {
-            //console.log(rsp.data);
-            setchannels(rsp.data.channels);
-            console.log(channels);
-            
 
-        }).catch(err => {
 
-        })
+    const weekend = () => {
+        var checkboxes = document.querySelectorAll('input[name=day]')
 
-    }, [])
-    const getData=(event)=>{
-        console.log("sent req");
-        axiosConfig.post("/channel/definedtrend",{id:id,month:month,year:year,day:day,start:start,finish:finish}).then(function(rsp){
-        
-            console.log({id:id,month:month,year:year,day:day,start:start,finish:finish});
-            console.log(rsp.data);    
-        setreach0(rsp.data.reach0);
-            setreachp(rsp.data.reachp);
-            settvrp(rsp.data.tvrp);
-            settvr0(rsp.data.tvr0);
-        },function(err){});
-        
-    }
-    
-    
-    const weekend=()=>{
-        var checkboxes=document.querySelectorAll('input[name=day]')
-    
         for (var i = 5; i < 7; i++) {
-            if(!(checkboxes[i].checked)){
-          checkboxes[i].click();}
-        }
-    }    
-    const weekday=()=>{
-        var checkboxes=document.querySelectorAll('input[name=day]')
-    
-        for (var i = 0; i < 5; i++) {
-            if(!(checkboxes[i].checked)){
-          checkboxes[i].click();}
+            if (!(checkboxes[i].checked)) {
+                checkboxes[i].click();
+            }
         }
     }
-    const checkbox=(val)=>{
-    var array = [];
-var checkboxes=document.querySelectorAll('input[name='+val+']:checked')
+    const weekday = () => {
+        var checkboxes = document.querySelectorAll('input[name=day]')
 
-for (var i = 0; i < checkboxes.length; i++) {
-array.push(checkboxes[i].value)
-}
-if(val=='month'){
-setmonth(array);
-console.log(month);
-}   
-if(val=='year'){
-setYear(array);
-console.log(year);
-}   
-if(val=='day'){
-setday(array);
-console.log(day);
-}      
+        for (var i = 0; i < 5; i++) {
+            if (!(checkboxes[i].checked)) {
+                checkboxes[i].click();
+            }
+        }
+    }
+    const checkbox = (val) => {
+        var array = [];
+        var checkboxes = document.querySelectorAll('input[name=' + val + ']:checked')
+
+        for (var i = 0; i < checkboxes.length; i++) {
+            array.push(checkboxes[i].value)
+        }
+        if (val == 'month') {
+            setmonth(array);
+            console.log(month);
+        }
+        if (val == 'year') {
+            setYear(array);
+            console.log(year);
+        }
+        if (val == 'day') {
+            setday(array);
+            console.log(day);
+        }
     }
     return (
         <div class="app-content content">
@@ -91,63 +70,48 @@ console.log(day);
                     <div class="card">
                         <div class="card-content">
                             <div class="card-body">
-                            <div class="row justify-content-between">
-                                    <div class="col-md-6">
-                                    <Select
-                                    placeholder="Select channel"
-                                    options={channels.map(channel => ({ label: channel.name, value: channel.id }))}
-                                    onChange={opt => setId(opt.value)}
-                                />
-                                    </div>
-                                    <div class="col-md-2">
-                                    <button class="btn btn-danger" onClick={getData}>Get Data</button>
-                                    </div>
-
-                                </div>
-                                <br/>
-                                <div class="h1 font-weight-bold">Select Ranges</div>
                                 <div class="row">
 
                                     <div class="col-md-3">
                                         <div class="h3 font-weight-bold">Select Year</div>
-                                        <input type="checkbox" name="year" value="2018" onChange={function(event){checkbox('year');}} />2018<br />
-                                        <input type="checkbox" name="year" value="2019" onChange={function(event){checkbox('year');}} />2019<br />
-                                        <input type="checkbox" name="year" value="2020" onChange={function(event){checkbox('year');}} />2020<br />
-                                        <input type="checkbox" name="year" value="2021" onChange={function(event){checkbox('year');}} />2021<br />
-                                        <input type="checkbox" name="year" value="2022" onChange={function(event){checkbox('year');}} />2022
+                                        <input type="checkbox" name="year" value="2018" onChange={function (event) { checkbox('year'); }} />2018<br />
+                                        <input type="checkbox" name="year" value="2019" onChange={function (event) { checkbox('year'); }} />2019<br />
+                                        <input type="checkbox" name="year" value="2020" onChange={function (event) { checkbox('year'); }} />2020<br />
+                                        <input type="checkbox" name="year" value="2021" onChange={function (event) { checkbox('year'); }} />2021<br />
+                                        <input type="checkbox" name="year" value="2022" onChange={function (event) { checkbox('year'); }} />2022
                                     </div>
                                     <div class="col-md-3">
                                         <div class="h3 font-weight-bold">Select Month</div>
-                                        <input type="checkbox" name="month" value="1" onChange={function(event){checkbox('month');}} />January<br />
-                                        <input type="checkbox" name="month" value="2" onChange={function(event){checkbox('month');}} />February<br />
-                                        <input type="checkbox" name="month" value="3" onChange={function(event){checkbox('month');}} />March<br />
-                                        <input type="checkbox" name="month" value="4" onChange={function(event){checkbox('month');}} />April<br />
-                                        <input type="checkbox" name="month" value="5" onChange={function(event){checkbox('month');}} />May<br />
-                                        <input type="checkbox" name="month" value="6" onChange={function(event){checkbox('month');}} />June<br />
-                                        <input type="checkbox" name="month" value="7" onChange={function(event){checkbox('month');}} />July<br />
-                                        <input type="checkbox" name="month" value="8" onChange={function(event){checkbox('month');}} />August<br />
-                                        <input type="checkbox" name="month" value="9" onChange={function(event){checkbox('month');}} />September<br />
-                                        <input type="checkbox" name="month" value="10" onChange={function(event){checkbox('month');}} />October<br />
-                                        <input type="checkbox" name="month" value="11" onChange={function(event){checkbox('month');}} />November<br />
-                                        <input type="checkbox" name="month" value="12" onChange={function(event){checkbox('month');}} />December
+                                        <input type="checkbox" name="month" value="1" onChange={function (event) { checkbox('month'); }} />January<br />
+                                        <input type="checkbox" name="month" value="2" onChange={function (event) { checkbox('month'); }} />February<br />
+                                        <input type="checkbox" name="month" value="3" onChange={function (event) { checkbox('month'); }} />March<br />
+                                        <input type="checkbox" name="month" value="4" onChange={function (event) { checkbox('month'); }} />April<br />
+                                        <input type="checkbox" name="month" value="5" onChange={function (event) { checkbox('month'); }} />May<br />
+                                        <input type="checkbox" name="month" value="6" onChange={function (event) { checkbox('month'); }} />June<br />
+                                        <input type="checkbox" name="month" value="7" onChange={function (event) { checkbox('month'); }} />July<br />
+                                        <input type="checkbox" name="month" value="8" onChange={function (event) { checkbox('month'); }} />August<br />
+                                        <input type="checkbox" name="month" value="9" onChange={function (event) { checkbox('month'); }} />September<br />
+                                        <input type="checkbox" name="month" value="10" onChange={function (event) { checkbox('month'); }} />October<br />
+                                        <input type="checkbox" name="month" value="11" onChange={function (event) { checkbox('month'); }} />November<br />
+                                        <input type="checkbox" name="month" value="12" onChange={function (event) { checkbox('month'); }} />December
                                     </div>
                                     <div class="col-md-3">
                                         <div class="h3 font-weight-bold">Select Day</div>
                                         <div class="row">
-                                        <div class="col-md-5">
-                                        <button onClick={weekday} class="btn btn-danger">Week Day</button>
+                                            <div class="col-md-5">
+                                                <button onClick={weekday} class="btn btn-danger">Week Day</button>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <button onClick={weekend} class="btn btn-danger">Weekend</button>
+                                            </div>
                                         </div>
-                                        <div class="col-md-3">
-                                        <button onClick={weekend} class="btn btn-danger">Weekend</button>
-                                        </div>
-                                        </div>
-                                        <input type="checkbox" name="day" value="6" onChange={function(event){checkbox('day');}} />Sunday<br />
-                                        <input type="checkbox" name="day" value="0" onChange={function(event){checkbox('day');}} />Monday<br />
-                                        <input type="checkbox" name="day" value="1" onChange={function(event){checkbox('day');}} />Tuesday<br />
-                                        <input type="checkbox" name="day" value="2" onChange={function(event){checkbox('day');}} />Wednesday<br />
-                                        <input type="checkbox" name="day" value="3" onChange={function(event){checkbox('day');}} />Thursday<br />
-                                        <input type="checkbox" name="day" value="4" onChange={function(event){checkbox('day');}} />Friday<br />
-                                        <input type="checkbox" name="day" value="5" onChange={function(event){checkbox('day');}} />Saturday<br />
+                                        <input type="checkbox" name="day" value="6" onChange={function (event) { checkbox('day'); }} />Sunday<br />
+                                        <input type="checkbox" name="day" value="0" onChange={function (event) { checkbox('day'); }} />Monday<br />
+                                        <input type="checkbox" name="day" value="1" onChange={function (event) { checkbox('day'); }} />Tuesday<br />
+                                        <input type="checkbox" name="day" value="2" onChange={function (event) { checkbox('day'); }} />Wednesday<br />
+                                        <input type="checkbox" name="day" value="3" onChange={function (event) { checkbox('day'); }} />Thursday<br />
+                                        <input type="checkbox" name="day" value="4" onChange={function (event) { checkbox('day'); }} />Friday<br />
+                                        <input type="checkbox" name="day" value="5" onChange={function (event) { checkbox('day'); }} />Saturday<br />
                                     </div>
                                     <div class="col-md-3">
                                         <div class="h3 font-weight-bold">Select Time</div>
@@ -155,19 +119,19 @@ console.log(day);
                                             <div class="col-md-6">
                                                 <fieldset class="form-group form-group-style">
                                                     <label for="start">Start Time</label>
-                                                    <input type="time" class="form-control" id="start" step="1" onChange={function(event){setstart(event.target.value);}}/>
+                                                    <input type="time" class="form-control" id="start" step="1" onChange={function (event) { setstart(event.target.value); }} />
                                                 </fieldset>
                                             </div>
                                             <div class="col-md-6">
                                                 <fieldset class="form-group form-group-style">
                                                     <label for="finish">Finish Time</label>
-                                                    <input type="time" class="form-control" id="finish" step="1" onChange={function(event){setfinish(event.target.value);}}/>
+                                                    <input type="time" class="form-control" id="finish" step="1" onChange={function (event) { setfinish(event.target.value); }} />
                                                 </fieldset>
                                             </div>
                                         </div>
                                         <div class="row">
-                                        
-                                        
+
+
                                         </div>
                                     </div>
 
@@ -176,39 +140,36 @@ console.log(day);
                             </div>
                         </div>
                     </div>
-                    
-                    <div class="card">
-                            <div class="card-content">
-                                <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="h6 font-weight-bold">Reach(%): </div>{reachp}%
-                                    </div>
-                                    <div class="col-md-6">
-                                    <div class="h6 font-weight-bold">Reach(000):  </div>{reach0}
-                                    </div>
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                        <div class="card">
-                            <div class="card-content">
-                                <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="h6 font-weight-bold">TVR(%): </div>{tvrp}%
-                                    </div>
-                                    <div class="col-md-6">
-                                    <div class="h6 font-weight-bold">TVR(000):  </div>{tvr0}
-                                    </div>
 
-                                </div>
-                            </div>
+                    <div class="row">
+
+
+                        <div class="col-md-6">
+                            <BarGraph title="Reach(%)" text="Channel vs Reach(%)" url="channel/definedtrendreachp" label="Reach of Channels" color="#28D094" get={false} credentials={{ month: month, year: year, day: day, start: start, finish: finish }} />
                         </div>
+                        <div class="col-md-6">
+                            <BarGraph title="Reach(000)" text="Channel vs Reach(000)" url="channel/definedtrendreach0" label="Reach of Channels" color="yellow" get={false} credentials={{ month: month, year: year, day: day, start: start, finish: finish }} />
+                        </div>
+
+
+
                     </div>
-                        
-                    
+                    <div class="row">
+
+
+                        <div class="col-md-6">
+                            <BarGraph title="TVR(%)" text="Channel vs TVR(%)" url="channel/definedtrendtvrp" label="TVR of Channels" color="#68D094" get={false} credentials={{ month: month, year: year, day: day, start: start, finish: finish }} />
+                        </div>
+                        <div class="col-md-6">
+                            <BarGraph title="TVR(000)" text="Channel vs TVR(000)" url="channel/definedtrendtvr0" label="TVR of Channels" color="#8D0394" get={false} credentials={{ month: month, year: year, day: day, start: start, finish: finish }} />
+                        </div>
+
+
+
+                    </div>
+
+
                 </div>
             </div>
         </div>
