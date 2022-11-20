d2c16f455cd5383ec8e204d5f5accece3c1295e9
shasabbir <shasabbir234@gmail.com>
2022-11-20 17:49:35 +0600

    program removed from program trp

--- src/Components/ProgramTrp/ProgramTrp.js
+++ src/Components/ProgramTrp/ProgramTrp.js
@@ -143,7 +143,7 @@ const ProgramTrp = () => {
         var greachp = 0;
         var gtvr0 = 0;
         var gtvrp = 0;
-        var csv = [["Channel", "Program", "Commercial", "Date", "Start", "Duration", "Time Watched", "Reach(000)", " Gross Reach(000)", "Reach(%)", "Gross Reach(%)", "TVR(000)", "Gross TVR(000)", "TVR(%)", "Gross TVR(%)"]];
+        var csv = [["Channel", "Commercial", "Date", "Start", "Duration", "Time Watched", "Reach(000)", " Gross Reach(000)", "Reach(%)", "Gross Reach(%)", "TVR(000)", "Gross TVR(000)", "TVR(%)", "Gross TVR(%)"]];
         //var all = allData;
         for (var i = 0; i < all.length; i++) {
 
@@ -152,7 +152,7 @@ const ProgramTrp = () => {
             gtvr0 = gtvr0 + all[i].tvr0;
             gtvrp = gtvrp + all[i].tvrp;
             csv.push([all[i].channel_name
-                , all[i].program, all[i].commercial_name
+                , all[i].commercial_name
                 , all[i].date, all[i].start
                 , all[i].duration, all[i].timewatched
                 , all[i].reach0, greach0
